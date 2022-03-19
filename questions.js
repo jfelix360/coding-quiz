6f2aeefded0f6e0dd88ac74ba1d1dc58a65bcdbe
jfelix360 <jfelix360@live.com>
2022-03-19 02:38:33 -0400

    Added scores.js which contains skeleton for object which will house questions, choices, and answers. Added html as seperate document for highscores page

---
+++ questions.js
@@ -0,0 +1,12 @@
+// Object containing questions, choices, and answers 
+
+var questions = [
+    //** */ FORMAT EXAMPLE, DELETE AFTER ADDING REAL QUESTIONS
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ['strings', 'booleans', 'alerts', 'numbers'],
+        answer: 'alerts'
+    },
+
+
+];
